9cf3c07db7105f302eb2a98169a3ec50f6e9bb60
drumesh44 <139769175+drumesh44@users.noreply.github.com>
2023-08-27 06:49:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preloadO() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('myFilterImage.png')
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
